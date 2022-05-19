ecf7842aa1916f9e5500338b3090f2532bbb2f10
Venkatareddy <venkatareddymvr1234@gmail.com>
2022-05-19 15:46:07 +0530

    Master change

--- src/Test/Example2.java
+++ src/Test/Example2.java
@@ -4,8 +4,8 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		System.out.println("Good Night");
-		System.out.println("2nd Branch");i
-		System.out.println("editing in GitBASH");
+		System.out.println("2nd Branch By Master");
+		System.out.println("editing in GitBASH updated by Master Branch");
 
 
 	}
